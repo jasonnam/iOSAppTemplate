4bb05dc1bae3fb110c6a6b22ba70a7b37d9aefb5
Jason Nam <contact@jasonnam.com>
2018-10-08 23:19:34 +0700

    Move SnapKit to Carthage

--- posthooks/init_summon_bin.sh
+++ posthooks/init_summon_bin.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 (cd $FLINT_OUTPUT_PATH && exec curl -L "https://raw.githubusercontent.com/summonbin/core/0.1.1/scripts/init.sh" | sh)
-(cd $FLINT_OUTPUT_PATH && exec echo "pod ruby 2.5.1" > "executable/debug/bin")
+(cd $FLINT_OUTPUT_PATH && exec echo "pod ruby 2.5.1" >> "executable/debug/bin")
+(cd $FLINT_OUTPUT_PATH && exec echo "carthage swift system https://github.com/Carthage/Carthage 0.31.0" >> "executable/debug/bin")
