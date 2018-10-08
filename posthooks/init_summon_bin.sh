a3b13f386542d4923b71b6e4b7428f052404b4f2
Jason Nam <contact@jasonnam.com>
2018-10-08 23:10:09 +0700

    Update summonbin

--- posthooks/init_summon_bin.sh
+++ posthooks/init_summon_bin.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-(cd $FLINT_OUTPUT_PATH && exec curl -L "https://raw.githubusercontent.com/summonbin/core/0.1.0/scripts/init.sh" | sh)
-(cd $FLINT_OUTPUT_PATH && exec echo "pod ruby 2.5.1" > "executable/default/bin")
+(cd $FLINT_OUTPUT_PATH && exec curl -L "https://raw.githubusercontent.com/summonbin/core/0.1.1/scripts/init.sh" | sh)
+(cd $FLINT_OUTPUT_PATH && exec echo "pod ruby 2.5.1" > "executable/debug/bin")
