ba12f023777c9dc00e5e51f04dea1fbd30e48a29
Jason Nam <contact@jasonnam.com>
2018-10-09 18:23:14 +0700

    Add Sourcery

--- posthooks/init_summon_bin.sh
+++ posthooks/init_summon_bin.sh
@@ -5,3 +5,4 @@
 (cd $FLINT_OUTPUT_PATH && exec echo "carthage swift system https://github.com/Carthage/Carthage 0.31.0" >> "executable/debug/bin")
 (cd $FLINT_OUTPUT_PATH && exec echo "bartycrouch binary download https://github.com/Flinesoft/BartyCrouch/releases/download/3.13.0/portable_bartycrouch.zip /" >> "executable/debug/bin")
 (cd $FLINT_OUTPUT_PATH && exec echo "swiftgen binary download https://github.com/SwiftGen/SwiftGen/releases/download/5.3.0/swiftgen-5.3.0.zip bin/" >> "executable/debug/bin")
+(cd $FLINT_OUTPUT_PATH && exec echo "sourcery swift system https://github.com/krzysztofzablocki/Sourcery 0.15.0" >> "executable/debug/bin")
