d6f78cc312f1dc0bd19eadccce67a51c51198260
Jason Nam <contact@jasonnam.com>
2019-04-27 23:03:31 +0700

    Add lanes

---
+++ template/fastlane/lanes/sync_code_signing.rb
@@ -0,0 +1,18 @@
+desc "Sync certificates and profiles"
+lane :sync_code_signing do
+  match(
+    app_identifier: ["--BundleID---Debug"],
+    type: "development",
+    readonly: true
+  )
+  match(
+    app_identifier: ["--BundleID---AdHoc"],
+    type: "adhoc",
+    readonly: true
+  )
+  match(
+    app_identifier: ["--BundleID--"],
+    type: "appstore",
+    readonly: true
+  )
+end
