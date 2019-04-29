2d91f892baf809e5eb29bb668e506c90e72b35fb
Jason Nam <contact@jasonnam.com>
2019-04-29 16:49:15 +0700

    Disable timeout

--- template/fastlane/lanes/install_certificates.rb
+++ template/fastlane/lanes/install_certificates.rb
@@ -2,7 +2,10 @@ desc "Install certificates and provisioning profiles"
 lane :install_certificates do
   create_keychain(
     name: "--Project Name--.keychain-db",
-    password: ENV["KEYCHAIN_PASSWORD"]
+    password: ENV["KEYCHAIN_PASSWORD"],
+    unlock: true,
+    timeout: false,
+    add_to_search_list: false
   )
   match(
     app_identifier: ["--BundleID---Debug"],
