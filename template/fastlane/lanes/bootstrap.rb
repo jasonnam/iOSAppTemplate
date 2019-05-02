39f1e1717b72efab0a8ef53f08671081ab1327aa
Jason Nam <contact@jasonnam.com>
2019-05-03 00:25:00 +0700

    Move SwiftGen and Sourcery to bootstrap

--- template/fastlane/lanes/bootstrap.rb
+++ template/fastlane/lanes/bootstrap.rb
@@ -5,4 +5,7 @@ lane :bootstrap do
   # install_certificates
   summonbin(command: "\"pod\" \"install\"")
   summonbin(command: "carthage bootstrap --platform ios --cache-builds")
+  summonbin(command: "swiftgen")
+  summonbin(command: "sourcery --config \"Sourcery/--Project Name--\"")
+  summonbin(command: "sourcery --config \"Sourcery/--Project Name--Tests\"")
 end
