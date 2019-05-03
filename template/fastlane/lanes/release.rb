ee197640ebd7ce5412744b79e623b066d44f347a
Jason Nam <contact@jasonnam.com>
2019-05-03 20:24:28 +0700

    Check branch and set version

--- template/fastlane/lanes/release.rb
+++ template/fastlane/lanes/release.rb
@@ -1,3 +1,31 @@
 desc "Release app on develop or release branches"
 lane :release do
+  if git_branch == "develop"
+    version = "[develop] #{last_git_commit[:abbreviated_commit_hash]}"
+  elsif git_branch.match(%r{release\/(\d|\.)+$}) && last_git_tag
+    if matches = last_git_tag.match(%r{release\/((\d|\.)+)\/\d+$})
+      version = matches.captures[0]
+    elsif
+      UI.message "Skip release on tag #{last_git_tag}"
+      next
+    end
+  else
+    UI.message "Skip release on branch #{git_branch}"
+    next
+  end
+  build_version = Time.now.to_i
+
+  UI.message "Version: #{version}"
+  UI.message "Build Version: #{build_version}"
+
+  set_version(
+    plist: "Sources/--Project Name--/Info-AdHoc.plist",
+    version: version,
+    build_version: build_version
+  )
+  set_version(
+    plist: "Sources/--Project Name--/Info-Release.plist",
+    version: version,
+    build_version: build_version
+  )
 end
