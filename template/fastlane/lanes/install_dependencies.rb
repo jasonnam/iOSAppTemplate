6fbb85b8bbbea8848a2a7973c551fa05f0ca4fac
Jason Nam <contact@jasonnam.com>
2019-05-04 10:00:00 +0700

    Remove Carthage

--- template/fastlane/lanes/install_dependencies.rb
+++ template/fastlane/lanes/install_dependencies.rb
@@ -1,5 +1,4 @@
 desc "Install dependencies"
 lane :install_dependencies do
   summonbin(command: "\"pod\" \"install\"")
-  summonbin(command: "carthage bootstrap --platform ios --cache-builds")
 end
