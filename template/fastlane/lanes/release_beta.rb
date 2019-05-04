a9b6136a4d41489345de07d1d8899f55a4f77a5f
Jason Nam <contact@jasonnam.com>
2019-05-04 09:29:32 +0700

    Build beta app

--- template/fastlane/lanes/release_beta.rb
+++ template/fastlane/lanes/release_beta.rb
@@ -1,3 +1,11 @@
 desc "Release beta"
 lane :release_beta do
+  gym(
+    scheme: "--Project Name-- AdHoc",
+    export_method: "ad-hoc",
+    export_options: {
+      compileBitcode: false
+    },
+    output_directory: "build/beta"
+  )
 end
