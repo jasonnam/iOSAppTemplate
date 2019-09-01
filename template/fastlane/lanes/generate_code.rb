70d348af6438c0cf7a15c5acd49e9715e2d87f1e
Jason Nam <contact@jasonnam.com>
2019-09-01 14:49:18 +0700

    Remove SwiftGen

--- template/fastlane/lanes/generate_code.rb
+++ template/fastlane/lanes/generate_code.rb
@@ -1,6 +1,5 @@
-desc "Generate code with SwiftGen and Sourcery"
+desc "Generate code with Sourcery"
 lane :generate_code do
-  summonbin(command: "swiftgen")
   summonbin(command: "sourcery --config \"Sourcery/--Project Name--\"")
   summonbin(command: "sourcery --config \"Sourcery/--Project Name--Tests\"")
 end
