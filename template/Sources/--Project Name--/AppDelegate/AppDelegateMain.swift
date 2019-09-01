050232bc25848f06b973d392bea42ef4a8c8ad66
Jason Nam <contact@jasonnam.com>
2019-09-01 16:45:56 +0700

    Raname Organization Name variable to ORGANIZATION_NAME

--- template/Sources/--Project Name--/AppDelegate/AppDelegateMain.swift
+++ template/Sources/--Project Name--/AppDelegate/AppDelegateMain.swift
@@ -3,7 +3,7 @@
 //  --Project Name--
 //
 //  Created by --Creator Name-- on --DATE:SHORT--.
-//  Copyright © --DATE:YEAR-- --Organization Name--. All rights reserved.
+//  Copyright © --DATE:YEAR-- --ORGANIZATION_NAME--. All rights reserved.
 //
 
 import AppDelegate
